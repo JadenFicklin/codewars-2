36c81876200a7032222c9d6559331bda5915f8de
JadenFicklin <jadendficklin@gmail.com>
2022-03-04 09:30:54 -0700

    boolean to word

---
+++ 4.2.js
@@ -0,0 +1,4 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+const boolToWordTwo = (b) => (b ? "Yes" : "No");
+
+console.log(boolToWordTwo(true));
